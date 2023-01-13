17112986329259fc6857d0168dfddfad66abf882
Idrusbashag <117631583+Idrusbashag@users.noreply.github.com>
2023-01-13 17:31:46 +0530

    rtyg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HashLocationStrategy,LocationStrategy } from '@angular/common';
 import {HttpClientModule,HTTP_INTERCEPTORS} from '@angular/common/http';
 import { AppRoutingModule ,routingComponents} from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -80,11 +79,9 @@ import { ViewoneuserComponent } from './admin/viewoneuser/viewoneuser.component'
     ToastrModule.forRoot(),
   ],
   providers: [AuthGuard,AdminGuard,{
-    // provide:HTTP_INTERCEPTORS,
-    // useClass:TokenInterceptorService,
-    multi:true,
-    provide:LocationStrategy,
-    useClass:HashLocationStrategy
+    provide:HTTP_INTERCEPTORS,
+    useClass:TokenInterceptorService,
+    multi:true
   }],
   bootstrap: [AppComponent]
 })
