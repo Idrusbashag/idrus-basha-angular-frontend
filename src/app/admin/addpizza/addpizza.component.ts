531b3ac553b2bf6694e4880e3442bfe1a54902c1
Idrusbashag <117631583+Idrusbashag@users.noreply.github.com>
2023-01-18 14:19:36 +0530

    commit

--- src/app/admin/addpizza/addpizza.component.ts
+++ src/app/admin/addpizza/addpizza.component.ts
@@ -49,14 +49,14 @@ export class AddpizzaComponent implements OnInit {
       return;
     }
     {
-    var formData:FormData = new FormData();
+    var formData = new FormData();
     formData.append('file', this.image);
     formData.append('pizzaname', f.controls.pizzaname.value);
     formData.append('pizzasize', f.controls.pizzasize.value);
     formData.append('pizzaprice', f.controls.pizzaprice.value);
     console.log(formData)
     }
-    this.http.post<any>('https://idrus-basha-food-order-backend.onrender.com/admin/addpizza', FormData,).subscribe(
+    this.http.post<any>('https://idrus-basha-food-order-backend.onrender.com/admin/addpizza', formData,).subscribe(
       (res) => {
         this.adminService.avail = true;
         this.adminService.msg = "Successfully Added a pizza!!!"
