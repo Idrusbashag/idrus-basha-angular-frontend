24c7a04e15155a4291e0fbe6d519bef912ba07a3
Idrusbashag <117631583+Idrusbashag@users.noreply.github.com>
2023-01-18 09:05:44 +0530

    commit

--- src/app/admin/addpizza/addpizza.component.ts
+++ src/app/admin/addpizza/addpizza.component.ts
@@ -45,7 +45,7 @@ export class AddpizzaComponent implements OnInit {
   onSubmit(f: NgForm) {
     if (!f.valid) {
       this.msg = "something went  wrong!!";
-      this.avail = false;
+      this.avail = true;
       return;
     }
     {
